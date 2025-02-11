458149c16a550a7591fe9c4177c854a93a872c49
Shahnas-P <shahanasp9@gmail.com>
2025-02-11 17:28:49 +0530

    Error handling

--- src/app.js
+++ src/app.js
@@ -2,41 +2,25 @@ const express = require('express')
 
 const app = express()
 
-const {auth} = require('./middleware/auth')
-const {userAuth}=require('./middleware/userAuth')
-app.use('/admin',auth)
 
-
-app.get('/admin/getAllData',(req,res)=>{
- 
-
-    res.send("Data fetched successfully!!")
-})
-
-app.delete('/admin/delete',(req,res)=>{
-
+app.use('/user',(req,res)=>{
    
-    
-    res.send("Data Deleted Successfully")
-})
-
-app.get("/user",userAuth,(req,res)=>{
-    console.log("Response consoled !! ");
-    res.send("user Response1 !!!")
+        throw new Error()
+        res.send(
+            "Data fetched successfully!!"
+        )
+  
    
-}
-)
 
-app.post("/user",userAuth,(req,res)=>{
-    console.log("Response consoled !! ");
-    res.send("user Response2 data saved !!!")
-   
 })
 
-app.post("/user/login",(req,res)=>{
-    console.log("Response consoled !! ");
-    res.send("user logined succesfully !!!")
+app.use('/',(err,req,res,next)=>{
+    if(err){
+        res.status(500).send("Some Error : Contact Support team")
+    }
 })
+
+
 
 app.listen(3000 ,()=>{
     console.log("Server running at port : 3000 ");
