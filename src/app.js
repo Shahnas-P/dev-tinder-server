1f2f747d24dec697af38a2a76b7fd12b1e5fa32d
Shahnas-P <shahanasp9@gmail.com>
2025-02-11 08:07:09 +0530

    Multiple router handler

--- src/app.js
+++ src/app.js
@@ -2,13 +2,29 @@ const express = require('express')
 
 const app = express()
 
-app.use("/hello",(req,res)=>{
-res.send("How are you Shanas Afsin")
-})
+app.use("/user",(req,res,next)=>{
+    console.log("Response consoled !! ");
+    // res.send("Response !!!")
+    next()
 
-app.use("/test",(req,res)=>{
-res.send("Helo from Server")
-})
+},[
+(req,res,next)=>{
+    console.log("Response 2 consoled !!");
+    
+    // res.send("Response 2 !!!")
+    next()
+},
+(req,res,next)=>{
+    console.log("Response 3 consoled !!!");
+    // res.send("Response 3  !!!")
+    next()
+}],
+(req,res,next)=>{
+    console.log("Response 4 consoled !!!");
+    res.send("Response 4")   
+
+}
+)
 
 app.listen(3000 ,()=>{
     console.log("Server running at port : 3000 ");
